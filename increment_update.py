fcaa2667e5729fb683c51297c6298b7bb92a2b7b
minecraftitsover90@gmail.com <Connor Jacobs>
2023-08-02 13:54:34 +0930

    fixed increment script

--- increment_update.py
+++ increment_update.py
@@ -1,16 +1,21 @@
 # increment_update.py
+import re
 
-# Read the current number from the file
+# Read the content of the update.py file
 with open('./update.py', 'r') as f:
     content = f.read()
-    current_number = int(content.strip().split('_')[-1])
 
-# Increment the number
-new_number = current_number + 1
+# Use regular expression to find the line with the file path
+pattern = r"with open\('\.\./output_files/output_(\d+)\.txt', 'r'\)"
+match = re.search(pattern, content)
 
-# Update the filename in the content
-updated_content = content.replace(f'output_{current_number}', f'output_{new_number}')
+if match:
+    current_number = int(match.group(1))  # Get the current number from the match
+    new_number = current_number + 1
+    updated_content = re.sub(pattern, f"with open('../output_files/output_{new_number}.txt', 'r')", content)
 
-# Write the updated content back to the file
-with open('./update.py', 'w') as f:
-    f.write(updated_content)
+    # Write the updated content back to the file
+    with open('./update.py', 'w') as f:
+        f.write(updated_content)
+else:
+    print("Pattern not found in the file.")
